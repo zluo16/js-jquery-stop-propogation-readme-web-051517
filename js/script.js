4051f55c19a4a24185bb2c3610258a6875ec03df
vicfriedman <victoria.m.friedman@gmail.com>
2016-01-27 16:22:24 -0500

    comments

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
 $(document).ready(function(){
 
   // toggle and delete
+  $('.course').on('click', function(){
+    $(this).find('.detail').slideToggle();
+  });
+
+  $('.course .delete').on('click', function(){
+    alert("about to delete");
+  });
+
+  //stop propogation
   // $('.course').on('click', function(){
   //   $(this).find('.detail').slideToggle();
   // });
 
-  // $('.course .delete').on('click', function(){
+  // $('.course .delete').on('click', function(event){
   //   alert("about to delete");
+  // event.stopPropagation();
+
   // });
-
-  //stop propogation
-   $('.course').on('click', function(){
-    $(this).find('.detail').slideToggle();
-  });
-
-  $('.course .delete').on('click', function(event){
-    alert("about to delete");
-  event.stopPropagation();
-
-  });
 });
